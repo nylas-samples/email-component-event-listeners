3a10121a58b3ca513d46e753282b68fb5fc5caaf
tjperry07 <tjperry07@users.noreply.github.com>
2022-04-13 11:15:28 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./styles.css";
+import "@nylas/components-email";
+
+export class App extends React.Component {
+  componentDidMount() {
+    const email = document.getElementsByTagName("nylas-email")[0];
+    email.addEventListener("manifestLoaded", function (e) {
+      console.log("manifest loaded: ", e.detail);
+    });
+    email.addEventListener("threadClicked", function (e) {
+      console.log("thread clicked: ", e.detail);
+    });
+    email.addEventListener("toggleThreadUnreadStatus", function (e) {
+      console.log("thread unread status toggled: ", e.detail);
+    });
+    email.addEventListener("threadDeleted", function (e) {
+      console.log("thread deleted: ", e.detail);
+    });
+    email.addEventListener("threadStarred", function (e) {
+      console.log("thread starred: ", e.detail);
+    });
+    email.addEventListener("messageClicked", function (e) {
+      console.log("message clicked: ", e.detail);
+    });
+    email.addEventListener("returnToMailbox", function (e) {
+      console.log("reuturn to mailbox clicked: ", e.detail);
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Nylas Email</h1>
+        <nylas-email
+          id="1709d4f8-920d-4f92-b0d1-7ea7a6f092b1"
+          thread_id="7ml84jdmfnw20sq59f30hirhe"
+          show_star={true}
+          show_contact_avatar="false"
+          show_expanded_email_view_onload={true}
+          click_action="default"
+        ></nylas-email>
+      </div>
+    );
+  }
+}
